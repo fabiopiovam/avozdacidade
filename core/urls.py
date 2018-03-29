fbae3758f4542bba62dc41e034894433cb49dfba
Fábio Piovam Elias <fabio@laborautonomo.org>
2018-03-28 21:39:36 -0300

    [chg] django2.0 structure. Verify requirements files to show installations

---
+++ core/urls.py
@@ -0,0 +1,50 @@
+"""core URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from django.conf import settings
+
+from . import views
+
+urlpatterns = [
+
+    # HOME
+    url(r'^$', views.Home.as_view(), name='home'),
+
+    # CONTACT FORM
+    url(r'^contact/', include('contact.urls')),
+
+    # CMS
+    url(r'^p/', include('cms.urls')),
+
+    # TAGS
+    url(r'^tags/', include('tags.urls')),
+
+    # CKEDITOR
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+
+    # ADMIN
+    url(r'^admin/', admin.site.urls),
+]
+
+if settings.LOCAL:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
